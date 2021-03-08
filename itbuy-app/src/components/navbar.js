e2be2e420d71f9745a0f2d2db5e86ce1db7d95d6
Maria Clara <44592405+clarascopetani@users.noreply.github.com>
2021-03-08 17:17:35 -0300

    desafio NavBar

--- itbuy-app/src/components/navbar.js
+++ itbuy-app/src/components/navbar.js
@@ -1,13 +1,15 @@
 import React from "react";
 
 export const NavBar = () => {
-    return <div>
+    return (
+    <div>
         <h3>ITBuy commerce</h3>
         <ul>
-            <li><a href="">Racks</a></li>
-            <li><a href="">Estabilizadores</a></li>
-            <li><a href="">UPS</a></li>
-            <li><a href="">Activos</a></li>
+            <li><a href=" ">Racks</a></li>
+            <li><a href=" ">Estabilizadores</a></li>
+            <li><a href=" ">UPS</a></li>
+            <li><a href=" ">Activos</a></li>
         </ul>
-    </div>;
-}
+    </div>
+    );
+};
