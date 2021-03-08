aa43d6a2a22ca279222c9af6544b06c4a43b35a6
Maria Clara <44592405+clarascopetani@users.noreply.github.com>
2021-03-08 16:46:21 -0300

    desafio 2

---
+++ itbuy-app/src/components/navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const NavBar = () => {
+    return <div>
+        <h3>ITBuy commerce</h3>
+        <ul>
+            <li><a href="">Racks</a></li>
+            <li><a href="">Estabilizadores</a></li>
+            <li><a href="">UPS</a></li>
+            <li><a href="">Activos</a></li>
+        </ul>
+    </div>;
+}
