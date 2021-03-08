17111bc705d776928eaeb1b03a8035106a63cad7
Maria Clara <44592405+clarascopetani@users.noreply.github.com>
2021-03-08 17:06:56 -0300

    Desafio 2

--- itbuy-app/src/App.js
+++ itbuy-app/src/App.js
@@ -1,11 +1,23 @@
+import logo from './logo.svg';
 import './App.css';
-import { NavBar } from './components/navbar';
 
 function App() {
   return (
     <div className="App">
-     <NavBar />
-     <h2>Acá va el contenido</h2>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
